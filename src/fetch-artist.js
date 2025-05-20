c94939c19922e6df4747ca06281218a46eaa8976
Tyechia Smith <tyechiasmith@gmail.com>
2025-05-20 15:29:28 -0400

    Updated api URL in fetch

--- src/fetch-artist.js
+++ src/fetch-artist.js
@@ -1,7 +1,7 @@
 
 
 export function fetchArtist(artist){
-  return fetch(`https://api.deezer.com/search?q=${artist}`)
+  return fetch(`https://cors-anywhere.herokuapp.com/https://api.deezer.com/search?q=${artist}`)
     .then(resp => resp.json())
 };
 
